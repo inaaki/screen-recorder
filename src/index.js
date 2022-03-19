23f63fbfcb5497ddef40b70782ce9b6431a62961
Inzamul Haque <inzamul119@gmail.com>
2022-03-20 01:15:53 +0600

    update UI & remove default menu

--- src/index.js
+++ src/index.js
@@ -22,6 +22,8 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  //remove default window
+  mainWindow.removeMenu();
 };
 
 // This method will be called when Electron has finished
